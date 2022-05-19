44d1cc5fd84125e4b80a2040998388705488b7a5
Lucas Almeida <lucasaclima03@gmail.com>
2022-05-19 14:10:54 -0300

    modularizando steps

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,10 +1,13 @@
-import { CloseButton } from "./CloseButton";
+import { CloseButton } from "../CloseButton";
 
-import bugImageUrl from "../assets/bug.svg";
-import ideaImageUrl from "../assets/idea.svg";
-import thoughtImageUrl from "../assets/thought.svg";
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: "Problema",
     image: {
@@ -28,24 +31,18 @@ const feedbackTypes = {
   },
 };
 
+export type FeedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto ">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-
-      <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-          return (
-            <button>
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title} </span>
-            </button>
-          );
-        })}
-      </div>
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep feedbackType={feedbackType} />
+      )}
 
       <footer className="text-xs text-neutral-400">
         Feito por{" "}
